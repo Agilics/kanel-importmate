f12dd64f8293356579c96fd1dddafecfa291d57c
moAllElite <mouhamedn347@gmail.com>
2025-08-12 14:43:48 +0000

    feature: Ajout de service et repertoire de projet importe

--- force-app/main/default/lwc/importProjectRecentComponent/importProjectRecentComponent.js
+++ force-app/main/default/lwc/importProjectRecentComponent/importProjectRecentComponent.js
@@ -2,4 +2,9 @@ import { LightningElement, api } from "lwc";
 
 export default class ImportProjectRecentComponent extends LightningElement {
   @api projects;
+
+  // check is project already imported
+   get hasNoProjects() {
+        return !( this.projects.data && this.projects.data.lenght > 0);
+    }
 }
--- force-app/main/default/lwc/mainComponent/mainComponent.js
+++ force-app/main/default/lwc/mainComponent/mainComponent.js
@@ -1,11 +1,37 @@
 import { LightningElement, wire, api } from "lwc";
-import getRecentsProjets from "@salesforce/apex/Namespace.ImportProjectController.getRecentsProjets";
+import getRecentsProjets from "@salesforce/apex/ImportProjectController.getRecentsProjets";
+// import static resources
+
+import { loadScript, loadStyle } from 'lightning/platformResourceLoader';  
+import fontawesome from '@salesforce/resourceUrl/fontawesome';
+
+
+
+
 export default class MainComponent extends LightningElement {
   @api limitor = 3;
 
-  @wire(getRecentsProjets, { limitor: "$limitor" }) projects; //3 latest import projects
+  @wire(getRecentsProjets, { limitor: "$limitor" }) importProjects; // 3 latest import projects
 
-  //check is project already imported
 
-  connectedCallback() {}
+  //load Fontawesome for Icons styles & scripts
+  connectedCallback() {
+    //code
+     Promise.all([
+            
+                //scripts
+                loadScript(this,fontawesome + 'fontawesome.min.js'),
+                loadScript(this,fontawesome + 'all.js'), 
+                loadScript(this,fontawesome + 'all.min.js'), 
+                
+                //load style for fontawesome 
+               loadStyle(this,fontawesome + '/css/all.min.css')
+
+
+            ]).then(() => {
+                    console.log("failed  to load fontawesome");
+                    ; 
+            });
+
+  }
 }
