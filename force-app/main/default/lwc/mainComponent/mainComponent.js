e375dcc08c793f5df35da13e03972284a6534a7d
moAllElite <mouhamedn347@gmail.com>
2025-08-12 14:43:48 +0000

    feature: Ajout de service et repertoire de projet importe

--- force-app/main/default/lwc/mainComponent/mainComponent.js
+++ force-app/main/default/lwc/mainComponent/mainComponent.js
@@ -1,9 +1,9 @@
-import { LightningElement, wire } from "lwc";
+import { LightningElement, wire, api } from "lwc";
 import getRecentsProjets from "@salesforce/apex/Namespace.ImportProjectController.getRecentsProjets";
 export default class MainComponent extends LightningElement {
-  projects;
+  @api limitor = 3;
 
-  @wire(getRecentsProjets) importProjects; //3 latest import projects
+  @wire(getRecentsProjets, { limitor: "$limitor" }) projects; //3 latest import projects
 
   //check is project already imported
 
