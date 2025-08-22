a66d39e6c63b259e4a63518cedb1b87601b93470
moAllElite <mouhamedn347@gmail.com>
2025-08-22 15:32:28 +0000

    feature: Ajout de la fonctionnalité consultation de projet récents

--- force-app/main/default/lwc/importProjectRecentComponent/importProjectRecentComponent.js
+++ force-app/main/default/lwc/importProjectRecentComponent/importProjectRecentComponent.js
@@ -1,10 +1,47 @@
-import { LightningElement, api } from "lwc";
-
+import { LightningElement, api, track } from "lwc";
+import searchProjetsById from "@salesforce/apex/ImportProjectController.searchProjetsById";
 export default class ImportProjectRecentComponent extends LightningElement {
+  //params
   @api projects;
-
-  // check is project already imported
-   get hasNoProjects() {
-        return !( this.projects.data && this.projects.data.lenght > 0);
-    }
+  @track projectId;
+  @track projet; // check is project already imported
+  get hasNoProjects() {
+    return !(this.projects.data && this.projects.data.length > 0);
+  } //on ferme la section projets recents et on affiche la section création de projet
+  handleShow() {
+    this.dispatchEvent(new CustomEvent("show"));
+  }
+  showModalBox() {
+    this.isShowModal = true;
+  }
+  hideModalBox() {
+    this.isShowModal = false;
+  }
+  //Affichage détail d'un projet importé via le modal SelectProjectComponent
+  async handleShowDetails(event) {
+    this.projectId = event.target.dataset.id;
+    searchProjetsById({ id: this.projectId })
+      .then((result) => {
+        console.log("Résultat brut Apex:", result);
+        this.project = result;
+        this.isShowModal = true;
+        /*return SelectProject.open({
+          size: "large",
+          description: "Détail du projet",
+          content: { content: result }
+        });*/
+      })
+      .then(() => {
+        console.log("Projet affiché:", this.project);
+        //   alert(JSON.stringify(this.project));
+      })
+      .catch((err) => {
+        console.error("Erreur Apex:", err);
+        //  alert(err.body ? err.body.message : err);
+      }); /*try { const result = await searchProjetsById({ id: this.projectId }); 
+      this.project = result; await SelectProjectComponent.open({ size: "large", description: "Détail du projet", content: { project: this.project } // passer via content }); } catch (error) { console.error('Erreur Apex:', error); }*/
+  } //fermeture la section projets récents et on affiche la section création de projets
+  openNewProject() {
+    this.showCreatorSection = true;
+  }
 }
--- force-app/main/default/lwc/mainComponent/mainComponent.js
+++ force-app/main/default/lwc/mainComponent/mainComponent.js
@@ -1,37 +1,8 @@
 import { LightningElement, wire, api } from "lwc";
 import getRecentsProjets from "@salesforce/apex/ImportProjectController.getRecentsProjets";
-// import static resources
-
-import { loadScript, loadStyle } from 'lightning/platformResourceLoader';  
-import fontawesome from '@salesforce/resourceUrl/fontawesome';
-
-
-
 
 export default class MainComponent extends LightningElement {
   @api limitor = 3;
 
   @wire(getRecentsProjets, { limitor: "$limitor" }) importProjects; // 3 latest import projects
-
-
-  //load Fontawesome for Icons styles & scripts
-  connectedCallback() {
-    //code
-     Promise.all([
-            
-                //scripts
-                loadScript(this,fontawesome + 'fontawesome.min.js'),
-                loadScript(this,fontawesome + 'all.js'), 
-                loadScript(this,fontawesome + 'all.min.js'), 
-                
-                //load style for fontawesome 
-               loadStyle(this,fontawesome + '/css/all.min.css')
-
-
-            ]).then(() => {
-                    console.log("failed  to load fontawesome");
-                    ; 
-            });
-
-  }
 }
