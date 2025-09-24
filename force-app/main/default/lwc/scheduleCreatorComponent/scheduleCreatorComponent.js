40514ffbfe4cd3da2c4a851df9bf9473d5ea3739
muhammadniang <mouhamed.niang@agilicis.com>
2025-09-24 08:22:22 +0000

    feat:Ajout du comboBox  de la liste de séléction de fréquence dans le formulaire de création de schedule

--- force-app/main/default/lwc/scheduleCreatorComponent/scheduleCreatorComponent.js
+++ force-app/main/default/lwc/scheduleCreatorComponent/scheduleCreatorComponent.js
@@ -1,17 +1,24 @@
-import { LightningElement,track } from 'lwc';
-
+import { LightningElement,track,wire } from 'lwc';
+import objectApiName from '@salesforce/schema/Schedule__c';
+import fieldApiName from '@salesforce/schema/Schedule__c.Frequency__c';
+import getPickListValues from "@salesforce/apex/ObjectMetadataController.getPickListValues";
 export default class ScheduleCreatorComponent extends LightningElement {
-    @track executionDate ; 
-    @track selectedFrequency;
+    @track executionDate ;    
+    @track picklistValues = [];
+    @track selectedFrequency; 
 
-    frequencyOptions = [
-        {
-            label:'Daily',value:'Daily',
-            label:'Weekly',value: 'Weekly',
-            label:'Monthly',value: 'Monthly'
+    @wire(getPickListValues, { objectApiName: '$objectApiName', fieldApiName: '$fieldApiName' })
+    wiredPicklistValues({ error, data }) {
+        if (data) {
+            this.picklistValues = Object.entries(data).map(([label, value]) => ({ label, value }));
+        } else if (error) {
+            console.error('Erreur lors de la récupération des valeurs de picklist : ', error);
         }
-    ];
+    }
 
+
+    
+    
     //Vérifie si la fréquence est sélectionnée
     get isSelectedFrequency(){
         return this.selectedFrequency != null && this.selectedFrequency.split("").length > 0;
