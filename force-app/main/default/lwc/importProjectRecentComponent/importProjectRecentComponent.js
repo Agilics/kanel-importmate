7c831cd663ab6896a9094b6c281ef060529fdae0
muhammadniang <mouhamed.niang@agilicis.com>
2025-10-20 10:12:16 +0000

    refactor:Updated ImportProjectController to include getCompatibleSObjects method and updated import statements in LWC components to use the new method.

--- force-app/main/default/lwc/importProjectRecentComponent/importProjectRecentComponent.js
+++ force-app/main/default/lwc/importProjectRecentComponent/importProjectRecentComponent.js
@@ -1,7 +1,5 @@
 import { LightningElement, api, track } from "lwc";
-import searchProjetById from "@salesforce/apex/ImportProjectController.searchProjetById";
-// importation du modal
-import DetailView from "c/projectDetailViewComponent";
+
 
 export default class ImportProjectRecentComponent extends LightningElement {
   //params
@@ -29,21 +27,7 @@ export default class ImportProjectRecentComponent extends LightningElement {
     this.dispatchEvent(new CustomEvent("show"));
   }
 
-  //Affichage  détail d'un projet importé via le modal DetailViewComponent
-  async handleShowDetails(event) {
-    //recherche le project via son id
-    searchProjetById({ id: event.target.dataset.id })
-      .then((result) => {
-        return DetailView.open({
-          size: "medium",
-          description: "Détail du projet",
-          project: result
-        });
-      })
-      .catch((err) => {
-        console.error("Erreur Apex:", err);
-      });
-  }
+  
 
   //fermeture la section projets récents et on affiche la section création de projets
   openNewProject() {
