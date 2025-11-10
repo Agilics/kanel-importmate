6428b94ba0fd783929028c4f6ec3a8a36f1accc9
muhammadniang <mouhamed.niang@agilicis.com>
2025-11-10 10:08:29 +0000

    fix datasource selector issues on lwc

--- force-app/main/default/lwc/dataSourceSelector/dataSourceSelector.js
+++ force-app/main/default/lwc/dataSourceSelector/dataSourceSelector.js
@@ -1,50 +1,7 @@
 import { LightningElement, api } from "lwc";
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class DataSourceSelector extends LightningElement {
-<<<<<<< HEAD
-  //data source selector card's parameters for file Upload
-  uploadTitle = "File Upload";
-  uploadSubtitle = "Import from CSV or Excel files";
-  uploadTextButton = "Choose File Upload";
-  uploadContents = [
-    {
-      Id: 1,
-      Name: "Support for CSV and Excel formats"
-    },
-    {
-      Id: 2,
-      Name: "Automatic delimiter detection"
-    },
-    {
-      Id: 3,
-      Name: "File preview and validation"
-    }
-  ];
-
-  //data source selector card's parameters for query builder
-  queryContents = [
-    {
-      Id: 1,
-      Name: "Visual query builder interface"
-    },
-    {
-      Id: 2,
-      Name: "Real-time query validation"
-    },
-    {
-      Id: 3,
-      Name: "Preview query results"
-    }
-  ];
-
-  queryTitle = "Salesforce Query";
-  querySubTitle = "Import from Salesforce using SOQL";
-
-  isUpload = true; // check if upload card
-  isNotUpload = !true;
-
-=======
->>>>>>> c521e35ecc0ed2c81e8f5f6eea48f7275f999e4d
+ 
   currentStep = 2;
   selectedSource = null; // "CSV" | "SOQL" | null
 
