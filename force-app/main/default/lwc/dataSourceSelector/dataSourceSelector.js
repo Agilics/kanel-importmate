1d69bf08b6c2188a12e2dbcb16fe1cc5e73ded52
muhammadniang <mouhamed.niang@agilicis.com>
2025-11-10 15:12:43 +0000

    fix:Updated data source selector component: updated dates, added file upload parameters and query builder parameters

--- force-app/main/default/lwc/dataSourceSelector/dataSourceSelector.js
+++ force-app/main/default/lwc/dataSourceSelector/dataSourceSelector.js
@@ -1,6 +1,47 @@
 import { LightningElement, api } from "lwc";
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class DataSourceSelector extends LightningElement {
+    //data source selector card's parameters for file Upload
+  uploadTitle = "File Upload";
+  uploadSubtitle = "Import from CSV or Excel files";
+  uploadTextButton = "Choose File Upload";
+  uploadContents = [
+    {
+      Id: 1,
+      Name: "Support for CSV and Excel formats"
+    },
+    {
+      Id: 2,
+      Name: "Automatic delimiter detection"
+    },
+    {
+      Id: 3,
+      Name: "File preview and validation"
+    }
+  ];
+
+  //data source selector card's parameters for query builder
+   queryContents = [
+    {
+      Id: 1,
+      Name: "Visual query builder interface"
+    },
+    {
+      Id: 2,
+      Name: "Real-time query validation"
+    },
+    {
+      Id: 3,
+      Name: "Preview query results"
+    }
+  ];
+
+  queryTitle = "Salesforce Query";
+  querySubTitle = "Import from Salesforce using SOQL";
+
+
+  isUpload = true; // check if upload card
+  isNotUpload = ! true;
   currentStep = 2;
   selectedSource = null; // "CSV" | "SOQL" | null
 
