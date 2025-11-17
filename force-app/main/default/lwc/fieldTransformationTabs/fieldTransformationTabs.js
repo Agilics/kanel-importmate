3afa435a8bb1bbf2e7c65301352809ca544c3ec4
muhammadniang <mouhamed.niang@agilicis.com>
2025-11-17 10:47:36 +0000

    feat :intégration de la page de transformation avec un modal permettant d'ajouter des règles

---
+++ force-app/main/default/lwc/fieldTransformationTabs/fieldTransformationTabs.js
@@ -0,0 +1,34 @@
+import { LightningElement, api } from "lwc";
+
+export default class FieldTransformationTabs extends LightningElement {
+  tabs = [
+    { label: "All Fields", value: "all", id: 1 },
+    { label: "Boolean Transformation", value: "boolean", id: 2 },
+    { label: "Case Transformation", value: "case", id: 3 },
+    { label: "Data Masking", value: "mask", id: 4 }
+  ];
+
+  @api activeTab = "all";
+
+  // Getter qui calcule les classes pour chaque onglet
+  get computedTabs() {
+    return this.tabs.map((tab) => ({
+      ...tab,
+      tabClass: tab.value === this.activeTab ? "tab active" : "tab"
+    }));
+  }
+
+  // Gestion du changement d'onglet
+  handleTabChange(event) {
+    const selectedTab = event.currentTarget.dataset.value;
+    this.activeTab = selectedTab;
+
+    this.dispatchEvent(
+      new CustomEvent("movetransformation", {
+        detail: { activetab: this.activeTab }
+      })
+    );
+
+    console.log("Onglet sélectionné:", this.activeTab);
+  }
+}
