63cfda32a768c56f1bf9abd111254e160d3ea684
moallelite <mouhamedn347@gmail.com>
2026-02-17 16:14:20 +0000

    feat: add custom labels for Project Card and update translations, including on project card

--- force-app/main/default/lwc/projectCard/projectCard.js
+++ force-app/main/default/lwc/projectCard/projectCard.js
@@ -1,8 +1,25 @@
 import { LightningElement, api } from 'lwc';
-import { navigateToPage } from 'c/utility'
+import { navigateToPage } from 'c/utility';
+import LOCALE from '@salesforce/i18n/lang';
+
+// import labels  
+import Target_Label from '@salesforce/label/c.ProjectCard_Meta_Target';
+import Records_Label from '@salesforce/label/c.ProjectCard_Meta_Records';
+import LastModified_Label from '@salesforce/label/c.ProjectCard_Meta_Modified';
+import Progress_Label from '@salesforce/label/c.ProjectCard_Progress_Label';
+import No_Description_Text from '@salesforce/label/c.ProjectCard_No_Description';
 
 export default class ProjectCard extends LightningElement {
     @api project;
+
+    get labels() {
+        return { 
+            target: Target_Label,
+            records: Records_Label,
+            lastModified: LastModified_Label,
+            progress: Progress_Label,
+        };
+    }
 
     get projectName() {
         return this.project?.Name || 'Unnamed Project';
@@ -13,17 +30,18 @@ export default class ProjectCard extends LightningElement {
     }
 
     get description() {
-        return this.project?.Description__c || 'No description';
+        return this.project?.Description__c || No_Description_Text;
     }
 
     get lastModifiedDate() {
         if (this.project?.LastModifiedDate) {
             const date = new Date(this.project.LastModifiedDate);
-            return date.toLocaleDateString('en-US', { 
+
+            return  new Intl.DateTimeFormat(LOCALE, { 
                 year: 'numeric', 
                 month: 'short', 
                 day: 'numeric' 
-            });
+            }).format(date);
         }
         return '';
     }
